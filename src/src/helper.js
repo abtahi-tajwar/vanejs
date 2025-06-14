9181f2054b553314060686e1f01b64037152e3cb
abtahi-tajwar <abtahitajwar@gmail.com>
2025-06-14 19:14:47 -0400

    Conveted libary source code into webpack modular project

--- src/src/helper.js
+++ src/src/helper.js
@@ -1,3 +1,4 @@
+import { EngineAttributes } from "./constants";
 export function parseEventQuery(query) {
   const result = [];
 
@@ -68,4 +69,79 @@ export function parseEventQuery(query) {
     return str;
   }
 }
+
+export function setNestedLevel(node, repeatAttr = 'data-repeat', levelAttr = 'data-repeat-level') {
+
+  let parent = node.parentElement;
+  let level = 0;
+
+  while (parent) {
+    if (parent.hasAttribute(repeatAttr)) {
+      level++;
+    }
+    parent = parent.parentElement;
+  }
+
+  node.setAttribute(levelAttr, level.toString());
+  return level;
+}
+
+export function evaluateCondition(expression, state) {
+  if (state) {
+    try {
+      // Replace all {variable} with real values from state
+      const populatedExpr = expression.replace(/\{(.*?)\}/g, (_, path) => {
+        const value = getValueByPath(state, path.trim());
+
+        // If value is string, wrap with quotes
+        if (typeof value === 'string') {
+          return `'${value}'`;
+        } else if (typeof value === 'object') {
+          return JSON.stringify(value); // or throw error
+        } else {
+          return value;
+        }
+      });
+
+
+      return Function(`return (${populatedExpr});`)();
+    } catch (e) {
+      console.warn("Failed to evaluate condition:", expression, e);
+      return false;
+    }
+
+  }
+};
+
+export function generateRandomID(length = 10) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=';
+  const array = new Uint8Array(length);
+  crypto.getRandomValues(array);
+  return Array.from(array, x => chars[x % chars.length]).join('');
+}
+
+export function removeCurlyBraces(str) {
+  str = str.trim(); // remove any surrounding whitespace
+
+  if (str.startsWith("{") && str.endsWith("}")) {
+    return str.slice(1, -1);
+  }
+
+  return str;
+}
+
+export function getValueByPath(obj, path) {
+  const normalizedPath = path.replace(/\[(\w+)\]/g, '.$1'); // arr[2] -> arr.2
+  const keys = normalizedPath.split('.');
+  let current = obj;
+
+  for (let key of keys) {
+    if (current === undefined || current === null) {
+      return undefined; // path not found
+    }
+    current = current[key];
+  }
+  return current;
+}
+
 
---
+++ src/src/utils.js
@@ -0,0 +1,12 @@
+import { EngineAttributes } from "./constants"
+import { prefix } from "./config";
+
+export function getTargetDOM(dom, id) {
+  return dom.querySelector(`[${EngineAttributes.ID}="${id}"]`)
+}
+
+export function makeAttr(name) {
+  return prefix ? `data-${prefix}-${name}` : `data-${name}`;
+}
+
+
